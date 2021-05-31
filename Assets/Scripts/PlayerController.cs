91f1a5cd561e864244a057c6288c5c7e8d840300
amber34911 <amber34911@gmail.com>
2021-05-31 22:59:56 +0800

    "Medium: Oncoming vehicles" completed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private float speed = 20.0f;
-    private float turnSpeed = 10.0f;
+    private float turnSpeed = 20.0f;
     private float horizontalInput;
     private float forwardInput;
 
